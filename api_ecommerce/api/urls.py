30972dcf49e5fbbe447ab3737c7c111a8369ed4c
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-03-17 19:36:30 -0300

    Criação dos métodos POST, GET, PATCH e DELETE de Endereços

--- api_ecommerce/api/urls.py
+++ api_ecommerce/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from api.views import UsuarioView # Import the view from views.py
+from api.views import UsuarioView, EnderecoView
 
 urlpatterns = [
     #path('user/<int:user_id>/', user_detail, name='user_detail'),
     #path('user/<int:user_id>/address/', address_detail(), name='address_detail'),
     #path('user/<int:user_id>/credit_card/', credit_card_detail, name='credit_card_detail'),
     path('usuarios/', UsuarioView.as_view(), name='lista-usuarios'), # Métodos GET(lista) e POST
-    path('usuarios/<int:pk>/', UsuarioView.as_view(), name='detalhes-usuario') # Métodos GET(detalhado e por id), PUT e DELETE
+    path('usuarios/<int:pk>/', UsuarioView.as_view(), name='detalhes-usuario'), # Métodos GET(detalhado e por id), PUT e DELETE
+    path('enderecos/', EnderecoView.as_view(), name='lista-enderecos'), # Métodos GET(lista) e POST
+    path('enderecos/<int:pk>/', EnderecoView.as_view(), name='detalhes-endereco') # Métodos GET(detalhado e por id), PUT e DELETE
 ]
 
--- api_ecommerce/api/views.py
+++ api_ecommerce/api/views.py
@@ -1,13 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from django.views import View
-from api.serializers import UsuarioSerializer
+from api.serializers import UsuarioSerializer, EnderecoSerializer
 # Criar requisições
-from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
 from .models import Usuario, Endereco, CartaoCredito
-import json
 from rest_framework import status
 
 
@@ -76,9 +71,6 @@ class UsuarioView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def delete(self, request, pk):
-        """
-        Delete a user by ID.
-        """
         try:
             usuario = Usuario.objects.get(pk=pk)
         except Usuario.DoesNotExist:
@@ -87,73 +79,61 @@ class UsuarioView(APIView):
                 status=status.HTTP_404_NOT_FOUND
             )
         
-        # Delete the user
         usuario.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class EnderecoView(APIView):
+    def post(self, request):
+        serializer = EnderecoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                # Se na requisição é enviado um ID de endereço, programa verifica se existe na base de dados e caso exista, retorna os dados.
+                endereco = Endereco.objects.get(pk=pk)
+                serializer = EnderecoSerializer(endereco)
+                return Response(serializer.data)
+            except Endereco.DoesNotExist:
+                return Response(
+                    {'error': 'Endereço não encontrado.'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+        
+        enderecos = Endereco.objects.all()
+        serializer = EnderecoSerializer(enderecos, many=True)
+        return Response(serializer.data)
 
-"""
-class UsuarioView():
-    def getAll(self, request):
-        products = Usuario.objects.all()
-        return render(request, 'products/list.html', {'products': products})
+    def patch(self, request, pk):
+        try:
+            endereco = Endereco.objects.get(pk=pk)
+        except Endereco.DoesNotExist:
+            return Response(
+                {'error': 'Endereço não encontrado.'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer = EnderecoSerializer(endereco, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def user_detail(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'GET':
-        data = {
-            'username': user.username,
-            'email': user.email,
-        }
-        return JsonResponse(data, )
-    elif request.method == 'POST':
-        data = json.loads(request.body)
-        user.username = data.get('username', user.username)
-        user.email = data.get('email', user.email)
-        user.save()
-        return JsonResponse({'status': 'success'}, status=201)
+    def delete(self, request, pk):
+        try:
+            endereco = Endereco.objects.get(pk=pk)
+        except Usuario.DoesNotExist:
+            return Response(
+                {'error': 'Endereço não encontrado.'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        endereco.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-def address_detail(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'GET':
-        addresses = Address.objects.filter(user=user)
-        data = [{
-            'street': addr.street,
-            'city': addr.city,
-            'state': addr.state,
-            'zip_code': addr.zip_code,
-            'country': addr.country,
-        } for addr in addresses]
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body)
-        address = Address.objects.create(
-            user=user,
-            street=data['street'],
-            city=data['city'],
-            state=data['state'],
-            zip_code=data['zip_code'],
-            country=data['country'],
-        )
-        return JsonResponse({'status': 'success', 'address_id': address.id}, status=201)
 
-def credit_card_detail(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'GET':
-        cards = CreditCard.objects.filter(user=user)
-        data = [{
-            'card_number': card.card_number,
-            'expiration_date': card.expiration_date,
-            'cvv': card.cvv,
-        } for card in cards]
-        return JsonResponse(data, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body)
-        card = CreditCard.objects.create(
-            user=user,
-            card_number=data['card_number'],
-            expiration_date=data['expiration_date'],
-            cvv=data['cvv'],
-        )
-        return JsonResponse({'status': 'success', 'card_id': card.id}, status=201)"
-"""
