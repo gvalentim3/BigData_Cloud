4f02fbee00dbb971097844d2930ba6455bea5503
Lucas Fernandes Mosqueira <lucas2002mkx@gmail.com>
2025-03-17 21:49:55 -0300

    Adicionando documentação Swagger as urls.py de api_ecommerce

--- api_ecommerce/api_ecommerce/urls.py
+++ api_ecommerce/api_ecommerce/urls.py
@@ -16,8 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="E-commerce API",
+        default_version='v1',
+        description="Documentação da API do e-commerce",
+        terms_of_service="https://www.seusite.com/termos/",
+        contact=openapi.Contact(email="suporte@seusite.com"),
+        license=openapi.License(name="Licença MIT"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Add this
+    path('api/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
