16d9227c15a5a0a40c9d9231c2ab16e162f7596f
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-03-18 00:00:34 -0300

    Mudança em serializer de usuário

--- api_ecommerce/api/serializers.py
+++ api_ecommerce/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import CartaoCredito,Usuario,Endereco, TipoEndereco
+from api.models import CartaoCredito,Usuario,Endereco,TipoEndereco
 
 class EnderecoSerializer (serializers.ModelSerializer):
     class Meta:
@@ -7,21 +7,6 @@ class EnderecoSerializer (serializers.ModelSerializer):
         fields = ['logradouro', 'complemento', 'bairro', 'cidade', 'estado', 'cep']
 
 class CartaoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CartaoCredito
-        fields = ['numero', 'dt_expiracao', 'cvv', 'saldo']
-
-class UsuarioSerializer (serializers.ModelSerializer):
-    cartoes = CartaoSerializer(many=True, read_only=True)
-    enderecos = EnderecoSerializer(many=True, read_only=True)
-    
-    class Meta:
-        model = Usuario
-        fields = ['nome', 'email', 'dt_nascimento', 'cpf', 'telefone', 'cartoes', 'enderecos']
-        read_only_fields = ['cpf']
-
-"""        
-class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartaoCredito
         fields = ['numero', 'dt_expiracao', 'cvv', 'saldo']
@@ -55,3 +40,12 @@ class UsuarioSerializer (serializers.ModelSerializer):
                 raise serializers.ValidationError(endereco_serializer.errors)
             
         return data
+
+
+"""        
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartaoCredito
+        fields = ['numero', 'dt_expiracao', 'cvv', 'saldo']
+
+"""
