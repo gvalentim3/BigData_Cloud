f507ea61e9e0e4a7b59bf82520dfa9ede7452cbb
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-04-06 06:03:38 -0300

    .

--- api_ecommerce/api_ecommerce/settings.py
+++ api_ecommerce/api_ecommerce/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',  # API app
+    'api_ecommerce.api.apps.ApiConfig',  # API app
     'drf_yasg',  # Swagger
 ]
 
