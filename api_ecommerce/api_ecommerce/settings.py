e8f3841a27e62968bba73bd9bd80b231f3c638cb
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-04-08 05:19:52 -0300

    Remove CustomAllowedHostsMiddleware from MIDDLEWARE in settings.py

--- api_ecommerce/api_ecommerce/settings.py
+++ api_ecommerce/api_ecommerce/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api_ecommerce.api.middleware.CustomAllowedHostsMiddleware'
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
