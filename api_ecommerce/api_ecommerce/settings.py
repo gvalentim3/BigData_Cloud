540dce662d216f36a2539fb613943303638acb71
Gustavo Valentim <gustavovalentim09@gmail.com>
2025-04-08 00:31:05 -0300

    mudança no requirements

--- api_ecommerce/api_ecommerce/settings.py
+++ api_ecommerce/api_ecommerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o#pdi%udpf=0g9n2wupct7yw#*!467rxjv(t1wenh3d$@e^%oa'
+SECRET_KEY = r'django-insecure-o#pdi%udpf=0g9n2wupct7yw#*!467rxjv(t1wenh3d$@e^%oa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
